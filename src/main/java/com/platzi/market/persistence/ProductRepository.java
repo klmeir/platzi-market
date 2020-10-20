d7711873fc89c0ffa436f1af06ca3ab61256363c
klmeir <caalmeir@gmail.com>
2020-10-20 13:56:37 -0500

    Implement the @Repository annotation

--- src/main/java/com/platzi/market/persistence/ProductRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductRepository.java
@@ -2,10 +2,12 @@ package com.platzi.market.persistence;
 
 import com.platzi.market.persistence.crud.ProductCrudRepository;
 import com.platzi.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductRepository {
 
     private ProductCrudRepository productCrudRepository;
@@ -20,6 +22,18 @@ public class ProductRepository {
 
     public Optional<List<Product>> getScarce(int stockQuantity) {
         return productCrudRepository.findByStockQuantityLessThanAndStatus( stockQuantity, true);
+    }
+
+    public Optional<Product> getProduct(long productId) {
+        return productCrudRepository.findById(productId);
+    }
+
+    public Product save(Product product) {
+        return productCrudRepository.save(product);
+    }
+
+    public void delete(long productId) {
+        productCrudRepository.deleteById(productId);
     }
 
 }
